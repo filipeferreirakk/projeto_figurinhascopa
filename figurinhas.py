fa45a89aa4f7d0417bab922ef2c14849d96f5f13
filipeferreirakk <filipegomesfrr@gmail.com>
2026-06-09 00:31:03 -0300

    Update figurinhas.py

--- figurinhas.py
+++ figurinhas.py
@@ -8,3 +8,9 @@ class Figurinha:
 
     def __str__(self):
         return "[" + str(self.id) + "] " + self.nome + " - " + self.pais + " - " + self.posicao + " - " + self.raridade
+
+
+class NodoLista:
+    def __init__(self, figurinha):
+        self.figurinha = figurinha
+        self.proximo = None
