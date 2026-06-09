34053c8cdf05f5bae947824278dcf9f886d88b81
filipeferreirakk <filipegomesfrr@gmail.com>
2026-06-09 00:22:19 -0300

    Update figurinhas.py

---
+++ figurinhas.py
@@ -0,0 +1,7 @@
+class Figurinha:
+    def __init__(self, id, nome, pais, posicao, raridade):
+        self.id = id
+        self.nome = nome
+        self.pais = pais
+        self.posicao = posicao
+        self.raridade = raridade
